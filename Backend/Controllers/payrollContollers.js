7f95a1b558793740ad6719d31cac7d65f915bfd1
JosephDigitty <olujosephdreal@yahoo.com>
2025-06-02 12:40:17 +0100

    update Payroll Controller

--- Backend/Controllers/payrollContollers.js
+++ Backend/Controllers/payrollContollers.js
@@ -160,9 +160,6 @@ const generateDefaultPayroll = async (req, res ) => {
             totalRecurringAllowance,
             netSalary
         })
-        }
-    
-
         newPayroll.push(payroll)
 
         if(newPayroll.length === 0 ) {
@@ -170,12 +167,138 @@ const generateDefaultPayroll = async (req, res ) => {
         }
         await Payroll.insertMany(newPayroll)
         res.status(201).json({success: true, message:"generated Payroll succesfully"})
-
+        }
     } catch (error) {
         console.log(error)
         res.status(500).json({success:false, error:"generate Payroll server Error"})
         
     }
 }
+const getallPayrollByDepartment = async () => {
+    try {
 
-export  {upsertPayroll, addPermSalarModifiers, generateDefaultPayroll}
+        const {payDate, department} = req.body
+        const period = new Date(payDate).toISOString().slice(0, 7)
+
+        if(!payDate) {
+            return res.status(400).json({success:false, error: "Paydate is required"})
+        }
+        if(!department) {
+            return res.status(400).json({success:false, error: "Department is required"})
+        }
+
+        const payrolls = await Payroll.find({period}).populate({
+            path: "employeeId",
+            populate: {
+                path: "department",
+                select: 'department_Name'
+            },
+            populate: {
+                path: "userId",
+                select: 'name'
+            }
+        })    
+
+        let filteredPayrollByDepartment = []
+
+        for (i = 0; i < payrolls.length; i++) {
+            if (payrolls[i].employeeId.department.department_id.toString() === department) {
+                filteredPayrollByDepartment.push(payrolls[i])
+            }
+        }
+
+        if(!filteredPayrollByDepartment) {
+            return res.status(404).json({success:false, error: "Payroll is not found"})
+        }
+        
+        
+        res.status(200).json({success: true, filteredPayrollByDepartment})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({success:false, error:"fetch Payroll server Error"}) 
+    }
+}
+const getallPayrollByGrade = async () => {
+    try {
+
+        const {payDate, grade} = req.body
+        const period = new Date(payDate).toISOString().slice(0, 7)
+
+        if(!payDate) {
+            return res.status(400).json({success:false, error: "Paydate is required"})
+        }
+        if(!grade) {
+            return res.status(400).json({success:false, error: "Department is required"})
+        }
+
+        const payrolls = await Payroll.find({period}).populate({
+            path: "employeeId",
+            populate: {
+                path: "grade",
+                select: 'gradeName'
+            },
+            populate: {
+                path: "userId",
+                select: 'name'
+            }
+        })    
+
+        let filteredPayrollByGrade = []
+
+        for (i = 0; i < payrolls.length; i++) {
+            if (payrolls[i].employeeId.grade.grade_id.toString() === department) {
+                filteredPayrollByGrade.push(payrolls[i])
+            }
+        }
+
+        if(!filteredPayrollByGrade) {
+            return res.status(404).json({success:false, error: "Payroll is not found"})
+        }
+        
+        
+        res.status(200).json({success: true, filteredPayrollByGrade})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({success:false, error:"fetch Payroll server Error"}) 
+    }
+}
+
+const getAllPayroll = async () => {
+    try {
+        const {payDate, department, grade} = req.body
+        const period = new Date(payDate).toISOString().slice(0, 7)
+
+        const payrolls = await Payroll.find({period}).populate({
+            path: "employeeId",
+            populate: {
+                path: "department",
+                select: 'department_Name'
+            },
+            populate: {
+                path: "grade",
+                select: 'gradeName'
+            },
+            populate: {
+                path: "userId",
+                select: 'name'
+            }
+        })
+
+        let filteredPayroll = []
+
+        for (i = 0; i < payrolls.length; i++) {
+            const emp = payrolls[i].employeeId
+            if(emp.department.department_id.toString() === department && emp.grade.grade_id.toString() === grade) {
+                filteredPayroll.push(payrolls[i])
+            }
+        }
+        if (!filteredPayroll) {
+            return res.status(404).json({success:false, error: "Payroll is not found"})
+        }
+        res.status(200).json({success: true, filteredPayroll})
+        } catch (error) {
+        console.log(error)
+        res.status(500).json({success:false, error:"fetch Payroll server Error"})
+    }
+}
+export  {upsertPayroll, addPermSalarModifiers, generateDefaultPayroll, getallPayrollByDepartment, getallPayrollByGrade, getAllPayroll }
